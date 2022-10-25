7aca67c659e312e5b7cdcfaef3e7d259fff0d49c
munna vail <98878071+devsayemmolla@users.noreply.github.com>
2022-10-26 00:50:06 +0600

    navbar done

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>navbar</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="info" variant="light">
+      <Container>
+        <Navbar.Brand>
+          <Link
+            className=" text-dark fw-bolder text-decoration-none"
+            to="/home"
+          >
+            <img style={{ width: "130px" }} src={logo} />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Link className="text-decoration-none text-white m-1" to="/home">
+              Home
+            </Link>
+            <Link className="text-decoration-none text-white m-1" to="/courses">
+              Courses
+            </Link>
+            <Link className="text-decoration-none text-white m-1" to="/faq">
+              FAQ
+            </Link>
+            <Link className="text-decoration-none text-white m-1" to="/blogs">
+              Blogs
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/routers/Route/Route.js
+++ src/routers/Route/Route.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../layouts/Main/Home";
 import Main from "../../layouts/Main/Main";
+import Blogs from "../../pages/Blogs/Blogs";
+import Courses from "../../pages/Courses/Courses";
+import Faq from "./Faq/Faq";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +17,18 @@ export const router = createBrowserRouter([
       {
         path: "/home",
         element: <Home></Home>,
+      },
+      {
+        path: "/courses",
+        element: <Courses></Courses>,
+      },
+      {
+        path: "/faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
       },
     ],
   },
